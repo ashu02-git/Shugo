3a2b06fe5b0da5dc6faec8ac264814dcb0695112
ashu02-git <shuhei.abe1107@gmail.com>
2021-01-11 03:00:48 +0000

    [Add] ゲストログイン機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # ゲスト登録情報
+  def self.guest
+    find_or_create_by!(email: 'guest@guest.com') do |user|
+      user.name = "Guest"
+      user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
+    end
+  end
 end
