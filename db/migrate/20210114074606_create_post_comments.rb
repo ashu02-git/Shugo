f63bf520f63bdfaf2edc4d335cb8cfb3059302b3
ashu02-git <shuhei.abe1107@gmail.com>
2021-01-14 10:26:58 +0000

    [Add] コメント機能

--- db/migrate/20210114074606_create_post_comments.rb
+++ db/migrate/20210114074606_create_post_comments.rb
@@ -2,7 +2,7 @@ class CreatePostComments < ActiveRecord::Migration[5.2]
   def change
     create_table :post_comments do |t|
       t.integer :post_id
-      t.indeger :user_id
+      t.integer :user_id
       t.text :comment, null: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_14_025930) do
+ActiveRecord::Schema.define(version: 2021_01_14_074606) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2021_01_14_025930) do
 
   create_table "hashtags", force: :cascade do |t|
     t.string "hash_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.text "comment", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
