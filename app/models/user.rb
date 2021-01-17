62516c05c741e99efd18b39719131fd6d805c2b8
ashu02-git <shuhei.abe1107@gmail.com>
2021-01-17 03:01:31 +0000

    [Fix] マイグレーションファイルdown

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy # User:Post = 1:N
   has_many :favorites, dependent: :destroy # User:Favorite = 1:N
   has_many :post_comments, dependent: :destroy # User:PostComment = 1:N
-  # フォロー用中間テーブル( User:User= N:N )
-  has_many :relationships
-  has_many :followings, through: :relationships, source: :follow
-  has_many :reverse_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
-  has_many :followers, through: :reverse_relationships, source: :user
+
   # ゲスト情報登録
   def self.guest
     find_or_create_by!(email: 'guest@guest.com') do |user|
@@ -23,21 +19,6 @@ class User < ApplicationRecord
   #退会済みか確認
   def active_for_authentication?
     super && (self.is_deleted == false)
-  end
-  # 他人ならフォローする
-  def follow(other_user)
-    unless self == other_user
-      self.relationships.find_or_create_by(follow_id: other_user.id)
-    end
-  end
-  # フォローを外す(フォローしていたら)
-  def unfollow(other_user)
-    relationship = self.relationships.find_by(follow_id: other_user.id)
-    relationship.destroy if relationship
-  end
-  # フォローしているか
-  def following?(other_user)
-    self.followings.include?(other_user)
   end
   # 画像attachment
   attachment :profile_image
